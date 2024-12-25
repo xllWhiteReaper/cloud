dcce2239a045d78cedb44b4497783295ce94fd4c
A-Cobra <conradbravina@gmail.com>
2024-12-25 08:40:38 -0500

    Adding a spring boot app to interact with an s3 bucket

---
+++ project1/app/aws_learning/src/main/java/com/a_cobra/aws_learning/services/HelloAWSService.java
@@ -0,0 +1,10 @@
+package com.a_cobra.aws_learning.services;
+
+import org.springframework.stereotype.Service;
+
+@Service()
+public class HelloAWSService {
+  public String getHelloAWS() {
+    return "Hello AWS";
+  }
+}
--- project1/infrastructure/main.tf
+++ project1/infrastructure/main.tf
@@ -7,3 +7,20 @@ resource "aws_instance" "be-server" {
     Name = "be-server-${count.index}"
   }
 }
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "spring-file"
+  aws_s3_bucket_acl = false
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
